2e3541ffd3dd84f6ac7fb30408a9778d0ca683fd
abudzynski <abudzynski@sii.pl>
2024-09-12 15:42:32 +0200

    fix

--- src/main/java/pl/sii/interview/model/InterviewItem.java
+++ src/main/java/pl/sii/interview/model/InterviewItem.java
@@ -1,5 +1,7 @@
 package pl.sii.interview.model;
 
+import java.util.Objects;
+
 public class InterviewItem {
     private final String question;
     private final String answer;
@@ -19,6 +21,19 @@ public class InterviewItem {
 
     public static Builder builder(){
         return new Builder();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof InterviewItem)) return false;
+        InterviewItem that = (InterviewItem) o;
+        return Objects.equals(question, that.question) && Objects.equals(answer, that.answer);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(question, answer);
     }
 
     public static class Builder {
